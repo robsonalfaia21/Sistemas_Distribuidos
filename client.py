ae3ab8f990adeab0a5ae41051d63313b6a945dcd
robsonalfaia21 <robsonalfaia21@gmail.com>
2018-04-12 15:28:07 -0300

    Robson

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ s = socket ()
 minhastr = "GET /rj/ HTTP/1.1\r\nHost: 10.10.13.1\r\nConnection: keep-alive\r\n\r\n"
 print (minhastr)
 
-meusbytes = str.encode (minhstr, "UFT-8")
+meusbytes = str.encode (minhastr, "UFT-8")
 print (meusbytes)
 
 servidor="10.10.13.1"
